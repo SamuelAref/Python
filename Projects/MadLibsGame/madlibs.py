43f981c25217bfb79031551b28c0bdd74716a037
Samuel Arefeaynie <samuelarefeaynie2@gmail.com>
2022-06-24 11:33:13 +0300

    Add files via upload

---
+++ Projects/MadLibsGame/madlibs.py
@@ -0,0 +1,7 @@
+name = input('insert a name: ')
+adjective = input('insert an adjective: ')
+color = input('insert a color: ')
+adverb = input('insert an adverb: ')
+
+
+print(f' The {adjective} {color} {name} jumped over the lazy dogs {adverb}')
